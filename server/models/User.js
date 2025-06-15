63bcf90ea5e6beb8c7e2dcae519f1b05c56b3cdf
sarathkumaar1 <sarathkumar16199@gmail.com>
2025-06-15 23:04:26 +0530

    modified

--- server/models/User.js
+++ server/models/User.js
@@ -10,6 +10,6 @@ const userSchema = mongoose.Schema({
 },{timestamps: true}
 );
 
-const User = mongoose.model("USer", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
